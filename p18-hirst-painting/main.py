82ba8f23a85755bd74eaed7da38933b9e5cda732
colungago <121251902+colungago@users.noreply.github.com>
2024-11-16 09:29:44 -0600

    finished project

--- p18-hirst-painting/main.py
+++ p18-hirst-painting/main.py
@@ -1,32 +1,28 @@
+import turtle as turtle_module
+import random
+from color_data import color_list
+
+turtle_module.colormode(255)
+turtle = turtle_module.Turtle()
+turtle.speed("fastest")
+turtle.penup()
+turtle.hideturtle()
+turtle.setheading(225)
+turtle.forward(300)
+turtle.setheading(0)
+number_of_dots = 100
 
 
-color_list = [
-    (236, 243, 240),
-    (243, 237, 242),
-    (199, 159, 114),
-    (69, 91, 129),
-    (148, 85, 52),
-    (218, 210, 115),
-    (136, 160, 193),
-    (27, 32, 47),
-    (179, 161, 35),
-    (58, 33, 22),
-    (184, 145, 164),
-    (123, 70, 93),
-    (137, 175, 153),
-    (76, 115, 78),
-    (143, 25, 15),
-    (61, 30, 41),
-    (187, 97, 82),
-    (120, 28, 43),
-    (46, 59, 94),
-    (99, 118, 172),
-    (178, 96, 114),
-    (33, 51, 44),
-    (99, 159, 85),
-    (66, 84, 23),
-    (215, 174, 192),
-    (217, 181, 172),
-    (218, 206, 7),
-    (159, 210, 191),
-]
+for dot_count in range(1, number_of_dots + 1):
+    turtle.dot(20, random.choice(color_list))
+    turtle.forward(50)
+    if dot_count % 10 == 0:
+        turtle.setheading(90)
+        turtle.forward(50)
+        turtle.setheading(180)
+        turtle.forward(500)
+        turtle.setheading(0)
+
+
+screen = turtle_module.Screen()
+screen.exitonclick()
