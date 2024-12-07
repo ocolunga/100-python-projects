9089cd2bff1077cec527529e8d7969531af775a4
ocolunga <oscar@ancile.dev>
2024-12-07 09:15:49 -0600

    added paddle bounce and miss logic

--- p22-pong/main.py
+++ p22-pong/main.py
@@ -6,12 +6,12 @@ import time
 
 screen = Screen()
 screen.bgcolor("black")
-screen.setup(width=600, height=600)
+screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 
-left_paddle = Paddle((-300, 0))
-right_paddle = Paddle((300, 0))
+left_paddle = Paddle((-350, 0))
+right_paddle = Paddle((350, 0))
 
 ball = Ball()
 
@@ -26,9 +26,29 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
     ball.move()
-    
+
+
+    # Detect collision with upper and lower walls
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
+
+
+    # Detect collision with paddles
+    if (
+        ball.distance(right_paddle) < 50
+        and ball.xcor() > 320
+        or ball.distance(left_paddle) < 50
+        and ball.xcor() < -320
+    ):
+        ball.bounce_x()
+        
+    # Detect when right paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        
+    # Detect when left paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
 
 
 screen.exitonclick()
