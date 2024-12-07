e2274b71be047370ca2146fb657c66b990ecc45c
ocolunga <oscar@ancile.dev>
2024-12-07 00:22:08 -0600

    added collision game over

--- p21-snake-game/main.py
+++ p21-snake-game/main.py
@@ -27,9 +27,20 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
-    
+
     if snake.head.distance(food) < 15:
         food.refresh()
         scoreboard.increase_score()
+
+    if (
+        snake.head.xcor() > 290
+        or snake.head.xcor() < -290
+        or snake.head.ycor() > 290
+        or snake.head.ycor() < -290
+    ):
+        
+        game_is_on = False
+        scoreboard.game_over()
+        # snake.reset()
 
 screen.exitonclick()
--- p21-snake-game/scoreboard.py
+++ p21-snake-game/scoreboard.py
@@ -3,6 +3,7 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Arial", 24, "normal")
 
+
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
@@ -21,8 +22,11 @@ class Scoreboard(Turtle):
             font=FONT,
         )
 
-
     def increase_score(self):
         self.score += 1
         self.clear()
         self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
