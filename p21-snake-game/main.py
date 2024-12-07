6ba6769f8e8a54e0a43ece07e0f7caee3652a236
ocolunga <oscar@ancile.dev>
2024-12-06 22:28:28 -0600

    segments

--- p21-snake-game/main.py
+++ p21-snake-game/main.py
@@ -5,16 +5,13 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
-segment1 = Turtle("square")
-segment1.color("white")
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
-segment2 = Turtle("square")
-segment2.color("white")
-segment2.goto(-20, 0)
-
-segment3 = Turtle("square")
-segment3.color("white")
-segment3.goto(-40, 0)
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position)
 
 
 
