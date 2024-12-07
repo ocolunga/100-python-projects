bb6daccddd3bbbb448893b6ee617e857071362fa
ocolunga <oscar@ancile.dev>
2024-12-06 23:00:26 -0600

    added snake class

--- p21-snake-game/main.py
+++ p21-snake-game/main.py
@@ -1,4 +1,6 @@
 from turtle import Screen, Turtle
+from snake import Snake
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -6,21 +8,14 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-
-segments = []
-
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+snake = Snake()
 
 game_is_on = True
-while game_is_on:
-    for segment in segments:
-        segment.forward(20)
 
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
 
 screen.exitonclick()
