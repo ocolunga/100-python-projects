058dc3ca8692531ce10a23999ab930998b055447
ocolunga <oscar@ancile.dev>
2024-12-07 00:34:03 -0600

    wrapped up main game

--- p21-snake-game/main.py
+++ p21-snake-game/main.py
@@ -30,6 +30,7 @@ while game_is_on:
 
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
 
     if (
@@ -38,9 +39,12 @@ while game_is_on:
         or snake.head.ycor() > 290
         or snake.head.ycor() < -290
     ):
-        
         game_is_on = False
         scoreboard.game_over()
-        # snake.reset()
+        
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
