83fffc3a71bc9c5fb196abaf96225dd2b1826c9a
ocolunga <oscar@ancile.dev>
2024-12-16 23:03:32 -0600

    completed project

--- p25-us-states/main.py
+++ p25-us-states/main.py
@@ -9,8 +9,25 @@ screen.addshape(image)
 turtle.shape(image)
 
 states_df = pd.read_csv("50_states.csv")
+all_states = states_df.state.to_list()
+guessed_states: list = []
 
-answer_state = screen.textinput(
-    title="Guess the State", prompt="What's another state's name?"
-)
-print(answer_state)
+while len(guessed_states) < 50:
+    answer_state = str(
+        screen.textinput(
+            title=f"{len(guessed_states)}/50 States Correct",
+            prompt="What's another state's name?",
+        )
+    ).title()
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = states_df[states_df.state == answer_state]
+        t.goto(state_data.x.item(), state_data.y.item())
+    t.write(answer_state)
+
+
+screen.exitonclick()
