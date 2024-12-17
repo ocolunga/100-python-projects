ae1783c8d210b693fe0efaabeb0554358b1a0f48
ocolunga <oscar@ancile.dev>
2024-12-16 22:42:02 -0600

    first commit

---
+++ p25-us-states/main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import turtle
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+states_df = pd.read_csv("50_states.csv")
+
+answer_state = screen.textinput(
+    title="Guess the State", prompt="What's another state's name?"
+)
+print(answer_state)
